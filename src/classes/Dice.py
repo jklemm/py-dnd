b59e7fe5b2e96201c1b24fb5f535df260467a855
Jorge Klemm <jorge@zumerata.com.br>
2014-07-22 14:08:48 -0300

    Change the Dice class to use Regexp.

--- src/classes/Dice.py
+++ src/classes/Dice.py
@@ -1,38 +1,42 @@
 
 from random import randint
+import re
+import logging
 
 class Dice(object):
 
 	def __init__ (self, formula):
-		self.times = 0
+		self.times = 1
 		self.sides = 0
 		self.bonus = 0
 		self.sliceFormula(formula)
 
 	def sliceFormula(self, formula):
-		array = formula.partition('d')
-		try:
-			self.times = int(array[0])
-		except:
-			self.times = 1
-		waste = array[2]
-		waste = waste.partition('+')
-		try:
-			self.sides = int(waste[0])
-		except:
-			self.sides = 4 # the minimum sides of a dice
-		try:
-			self.bonus = int(waste[2])
-		except:
-			self.bonus = 0
+		regexp = re.compile(r'(\d+)*d(\d+)*[+]*(\d+)*')
+		m = re.match(regexp, formula)
+		if m:
+			try:
+				self.times = int(m.group(1))
+			except:
+				self.times = 1
+
+			try:
+				self.sides = int(m.group(2))
+			except:
+				self.sides = 4 # the minimum sides of a dice
+
+			try:
+				self.bonus = int(m.group(3))
+			except:
+				self.bonus = 0
 
 	def throwDice(self):
 		result = 0
 		for i in range(0,self.times):
 			randomnumber = randint(1,self.sides)
-			print (str(randomnumber) + " + ")
+			logging.debug (str(randomnumber) + " + ")
 			result += randomnumber
-		print (str(self.bonus) + " = ")
+		logging.debug (str(self.bonus) + " = ")
 		return result + self.bonus
 
 # Manual Tests simple side dice
