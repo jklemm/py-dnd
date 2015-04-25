8156a17c51b3c16643141671f9c5d6efdcc5bd5b
jklemm <jorge@meuspedidos.com.br>
2015-04-25 19:08:34 -0300

    Added lists of Deities, Scripts and Languages

--- core/entities/character_utils.py
+++ core/entities/character_utils.py
@@ -58,3 +58,46 @@ class AlignmentList(object):
     EVIL = 'EVIL'
     CHAOTIC_EVIL = 'CHAOTIC_EVIL'
 
+
+class DeitiesList(object):
+    ASMODEUS = AlignmentList.EVIL
+    AVANDRA = AlignmentList.GOOD
+    BAHAMUT = AlignmentList.LAWFUL_GOOD
+    BANE = AlignmentList.EVIL
+    CORELLON = AlignmentList.UNALIGNED
+    ERATHIS = AlignmentList.UNALIGNED
+    GRUUMSH = AlignmentList.CHAOTIC_EVIL
+    IOUN = AlignmentList.UNALIGNED
+    KORD = AlignmentList.UNALIGNED
+    LOLTH = AlignmentList.CHAOTIC_EVIL
+    MELORA = AlignmentList.UNALIGNED
+    MORADIN = AlignmentList.LAWFUL_GOOD
+    PELOR = AlignmentList.GOOD
+    SEHANINE = AlignmentList.UNALIGNED
+    THE_RAVEN_QUEEN = AlignmentList.UNALIGNED
+    TIAMAT = AlignmentList.EVIL
+    TOROG = AlignmentList.EVIL
+    VECNA = AlignmentList.EVIL
+    ZEHIR = AlignmentList.EVIL
+
+
+class ScriptList(object):
+    COMMON = 'COMMON'
+    RELLANIC = 'RELLANIC'
+    IOKHARIC = 'IOKHARIC'
+    DAVEK = 'DAVEK'
+    BARAZHAD = 'BARAZHAD'
+    SUPERNAL = 'SUPERNAL'
+
+
+class LanguageList(object):
+    COMMON = ScriptList.COMMON
+    DEEP_SPEECH = ScriptList.RELLANIC
+    DRACONIC = ScriptList.IOKHARIC
+    DWARVEN = ScriptList.DAVEK
+    ELVEN = ScriptList.RELLANIC
+    GIANT = ScriptList.DAVEK
+    GOBLIN = ScriptList.COMMON
+    PRIMORDIAL = ScriptList.BARAZHAD
+    SUPERNA = ScriptList.SUPERNAL
+    ABYSSAL = ScriptList.BARAZHAD
